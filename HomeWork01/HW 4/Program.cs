8a975e8e1c4b2bd02e3813921627d217b279895c
Tatiana <tdz1604@gmail.com>
2022-11-18 14:02:55 +0300

    004

--- HomeWork01/HW 4/Program.cs
+++ HomeWork01/HW 4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the First Number");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Second Number");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the Third Number");
+int numberThird = Convert.ToInt32(Console.ReadLine());
