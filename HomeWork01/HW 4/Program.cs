dce2821d92f5677945268ae0221e7ebe55f20e66
Tatiana <tdz1604@gmail.com>
2022-11-18 14:29:02 +0300

    hw 4 done

--- HomeWork01/HW 4/Program.cs
+++ HomeWork01/HW 4/Program.cs
@@ -4,3 +4,12 @@ Console.WriteLine("Enter the Second Number");
 int numberSecond = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the Third Number");
 int numberThird = Convert.ToInt32(Console.ReadLine());
+int max = numberFirst;
+
+if(numberFirst > max) max = numberFirst;
+if(numberSecond > max) max = numberSecond;
+if(numberThird > max) max = numberThird;
+
+Console.Write($"max number = ");
+Console.WriteLine(max);
+
