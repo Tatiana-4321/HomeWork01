84cd227a08fd03332ba8faf5a15a7153ad1668f4
Tatiana <tdz1604@gmail.com>
2022-11-19 11:29:55 +0300

    Add HW 8

---
+++ HomeWork01/HW 8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter the Number");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (number >= count)
+{
+    if (count % 2 == 0)
+    Console.Write(count + " ");
+    count ++;
+}
